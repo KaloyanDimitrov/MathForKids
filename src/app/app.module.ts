471ef97ad8c9790db0ec0d97ff1111f554a51d99
KaloyanDimitrov <kaloyan.dimitrov00@gmail.com>
2020-08-15 19:47:26 +0200

    Start of project Login page & Header added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-
+import { NgModule, ReflectiveInjector } from '@angular/core';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { appRoutes } from './routes';
+import { LoginComponent } from './login/login.component';
+import { AboutComponent } from './about/about.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { ActivatedRouteSnapshot, DetachedRouteHandle, RouteReuseStrategy } from '@angular/router';
+import { SettingsComponent } from './settings/settings.component';
+import { PlayComponent } from './play/play.component';
 
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      HeaderComponent,
+      LoginComponent,
+      AboutComponent,
+      SignUpComponent,
+      SettingsComponent,
+      PlayComponent
+   ],
+   imports: [
+      BrowserModule,
+      appRoutes
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
---
+++ src/app/routes.ts
@@ -0,0 +1,17 @@
+import { ModuleWithProviders } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { AboutComponent } from './about/about.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { PlayComponent } from './play/play.component';
+import { SettingsComponent } from './settings/settings.component';
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'signup', component: SignUpComponent },
+  { path: 'play', component: PlayComponent },
+  { path: 'settings', component: SettingsComponent },
+];
+
+export const appRoutes: ModuleWithProviders = RouterModule.forRoot(routes);
